b4cb8a5436260182043918690b56e37b3cb847aa
redao <redao@192.168.1.14>
2019-05-29 00:40:40 +0000

    initial maven project

---
+++ src/main/java/com/codehelp/CodehelpApplication.java
@@ -0,0 +1,13 @@
+package com.codehelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodehelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodehelpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codehelp/CodehelpApplicationTests.java
@@ -0,0 +1,14 @@
+/*package com.codehelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodehelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
