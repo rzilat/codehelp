dbb8708b0ad44df0dfdeb4de4f229320c49cb0bb
redao <redao@192.168.1.4>
2019-07-31 00:08:55 +0100

    change test and pom

--- src/test/java/com/codehelp/CodehelpApplicationTest.java
+++ src/test/java/com/codehelp/CodehelpApplicationTest.java
@@ -1,11 +1,18 @@
 package com.codehelp;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class CodehelpApplicationTest {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception{
 	}
 
 }
